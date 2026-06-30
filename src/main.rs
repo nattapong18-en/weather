3b7fa00c18dcf672b695e5e4b57e165381a1571b
nattapong18-en <664110310060@npu.ac.th>
2026-07-01 00:16:14 +0700

    add how to use

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 mod weather;
 use weather::fetch;
 
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
     let args: Vec<String> = std::env::args().collect();
-    let city = &args[1];
+    let city = match args.get(1) {
+        Some(city) => city,
+        None => {
+            eprintln!("Enter the city");
+            eprintln!("Ex. cargo run -- Bangkok");
+            return Ok(());
+        }
+    };
     let weather = fetch(city)?;
     let condition = &weather.current_condition[0];
     let desc = &condition.weather_desc[0].value;
@@ -13,6 +18,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("🌤 {}", desc);
     println!("🌡 {}°C", condition.temp_c);
     println!("💧 {}%", condition.humidity);
-   
+
     Ok(())
 }
